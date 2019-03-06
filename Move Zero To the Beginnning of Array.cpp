33bf83d81a6b5cd2c9daea7ee8a285b67b063a0f
salmalifias <salmalifia.shafira@gmail.com>
2019-03-06 10:39:03 +0700

    strukdat-02

---
+++ Move Zero To the Beginnning of Array.cpp
@@ -0,0 +1,24 @@
+/*
+Nama           : Salma Alifia Shafira
+NPM            : 140810180058
+Kelas		   : B
+Tanggal Buat   : 06 Maret 2019
+*/
+
+#include <iostream>
+using namespace std;
+
+void moveZeroToFront(int arr[], int n{})
+void input(int (&arr)[100], int& n){}
+void output(int arr[], int n){}
+
+int main(){
+	int arr[100];
+	int n;
+	cout<<"Masukkan panjang data : "; cin>>i;
+	cout<<"Masukkan data cth [1 2 3 4 5] : "; cin>>x[i];
+	input(arr,n);
+	moveZeroToFront(arr,n);
+	output(arr, n);
+	cout<<"Hasil : "<<
+}
